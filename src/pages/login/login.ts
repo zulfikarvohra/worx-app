b1d554623a3c5d4d8d86ffbdeb1f0803eddd74a5
Zulfikar ali <zulfikarali@Zulfikars-MacBook-Pro.local>
2018-11-14 21:52:07 +0300

    change in login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { NgForm } from '@angular/forms';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
@@ -22,8 +16,10 @@ export class LoginPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
-  navigateToHome(){
-    this.navCtrl.setRoot(HomePage,{}, {animate: true, direction: 'forward'});
+  navigateToHome() {
+    this.navCtrl.setRoot(HomePage, {}, { animate: true, direction: 'forward' });
   }
-
+  onSignIn(form: NgForm) {
+    console.log(form.value);
+  }
 }
